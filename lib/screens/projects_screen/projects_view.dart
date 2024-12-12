ee37d238ba4eb29745763daebffb4c92097159dc
Faisal Nazir <faisalnazir28@gmail.com>
2024-12-12 16:18:34 +0500

    Projects Screen populated with all projects

--- lib/screens/projects_screen/projects_view.dart
+++ lib/screens/projects_screen/projects_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:portfolio_admin/controllers/projects_controller.dart';
 
 class ProjectsView extends StatefulWidget {
   const ProjectsView({super.key});
@@ -8,10 +11,338 @@ class ProjectsView extends StatefulWidget {
 }
 
 class _ProjectsViewState extends State<ProjectsView> {
+  final projectsController = Get.find<ProjectsController>();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Projects View'),
+    return Obx(() {
+      return Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          constraints: BoxConstraints(
+                            maxHeight: MediaQuery.of(context).size.height * 0.82,
+                          ),
+                          child: GridView.builder(
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              crossAxisSpacing: 25,
+                              mainAxisSpacing: 25,
+                              childAspectRatio: 1.8775,
+                            ),
+                            itemCount: projectsController.allProjects.length,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                overlayColor: WidgetStateProperty.all(Colors.transparent),
+                                onTap: () => setState(() {
+                                  // selectedProjectIndex = index;
+                                  // clientDetailsView = true;
+                                }),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(color: Colors.black45, width: 1),
+                                  ),
+                                  padding: const EdgeInsets.all(20),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        children: [
+                                          CircleAvatar(
+                                            radius: 30,
+                                            backgroundImage:
+                                                NetworkImage(projectsController.allProjects[index].mainImage),
+                                          ),
+                                          const SizedBox(
+                                            width: 20,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                projectsController.allProjects[index].projectName,
+                                                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                                              ),
+                                              const SizedBox(
+                                                height: 5,
+                                              ),
+                                              Text(
+                                                projectsController.allProjects[index].clientName,
+                                                style: const TextStyle(fontSize: 12),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 15),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            projectsController.allProjects[index].projectIsCompleted
+                                                ? 'Completed'
+                                                : 'Incomplete',
+                                            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                                          ),
+                                          Text(
+                                            " - ${projectsController.allProjects[index].projectStatus}",
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            projectsController.allProjects[index].projectType,
+                                            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                                          ),
+                                          Text(
+                                            " - ${projectsController.allProjects[index].projectPlatform}",
+                                            style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                                          ),
+                                        ],
+                                      ),
+                                      const SizedBox(height: 15),
+                                      const Divider(height: 5),
+                                      const SizedBox(height: 15),
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                projectsController.allProjects[index].projectDate,
+                                                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                                              ),
+                                              const SizedBox(height: 5),
+                                              const Text(
+                                                "deadline",
+                                                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                                              ),
+                                            ],
+                                          ),
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                projectsController.allProjects[index].projectBudget,
+                                                style: const TextStyle(
+                                                    fontSize: 15,
+                                                    fontWeight: FontWeight.w500,
+                                                    color: Colors.deepOrangeAccent),
+                                              ),
+                                              const SizedBox(height: 5),
+                                              const Text(
+                                                'Total Budget',
+                                                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 40,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * .15,
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height * 0.82,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.black45, width: 1),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          child: const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Total balance",
+                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                "\$0",
+                                style: TextStyle(fontSize: 19, color: Colors.green, fontWeight: FontWeight.w600),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            border: Border.all(color: Colors.black45, width: 1),
+                          ),
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    "Total Projects",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                                  ),
+                                  Text(
+                                    projectsController.allProjects.length.toString(),
+                                    style: const TextStyle(fontSize: 19),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    "Active Projects",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                                  ),
+                                  Text(
+                                    projectsController.allProjects
+                                        .where((e) => e.projectIsCompleted == false)
+                                        .length
+                                        .toString(),
+                                    style: const TextStyle(fontSize: 19),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    "Inactive Projects",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                                  ),
+                                  Text(
+                                    projectsController.allProjects
+                                        .where((e) => e.projectIsCompleted == true)
+                                        .length
+                                        .toString(),
+                                    style: const TextStyle(fontSize: 19),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        addButton(
+                            icon: Ionicons.person_add_outline,
+                            title: "New Client",
+                            onTap: () {
+                              // newClientView = true;
+                            }),
+                        addButton(icon: Ionicons.code_working_outline, title: "New Project", onTap: () {}),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      );
+    });
+  }
+
+  Widget addButton({
+    required IconData icon,
+    required String title,
+    required Function() onTap,
+  }) {
+    return InkWell(
+      overlayColor: WidgetStateProperty.all(Colors.transparent),
+      onTap: () {
+        setState(() {
+          onTap();
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          border: Border.all(color: Colors.black45, width: 1),
+        ),
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.only(bottom: 30),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              size: 30,
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
