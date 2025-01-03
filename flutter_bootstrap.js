748b4f65b27e52468c74bdb7406a530694b93e30
Faisal Nazir VM <fnvm@soloinsight.com>
2025-01-03 15:56:17 +0500

    deployed v4.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3892948850"
+    serviceWorkerVersion: "1665709669"
   }
 });
